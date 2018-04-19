bc11d9b4a85ad1531dcc45426c08bde096a7c116
Narongwate Sangsakul <goffity@gmail.com>
2018-04-19 17:39:08 +0700

    Added android UI testcase

--- app/src/main/java/com/goffity/mobile/android/demo/retrofitdemo/activity/MainActivity.java
+++ app/src/main/java/com/goffity/mobile/android/demo/retrofitdemo/activity/MainActivity.java
@@ -1,15 +1,16 @@
 package com.goffity.mobile.android.demo.retrofitdemo.activity;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
+
 import com.goffity.mobile.android.demo.retrofitdemo.R;
 import com.goffity.mobile.android.demo.retrofitdemo.model.MemberResponse;
 import com.goffity.mobile.android.demo.retrofitdemo.network.retrofit.RetrofitImplBuilder;
 import com.goffity.mobile.android.demo.retrofitdemo.network.retrofit.impl.LoginServices;
 import com.goffity.mobile.android.demo.retrofitdemo.network.retrofit.member.MemberAPI;
-import java.io.IOException;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -17,61 +18,78 @@ import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
 
-  private final String TAG = MainActivity.class.getSimpleName();
-  MemberResponse memberResponse;
+    private final String TAG = MainActivity.class.getSimpleName();
+    MemberResponse memberResponse;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    LoginServices loginServices = new LoginServices();
-    loginServices.start("goffity@gmail.com", "password");
+        LoginServices loginServices = new LoginServices();
+        loginServices.start("goffity@gmail.com", "password");
 
-    Retrofit retrofit = new RetrofitImplBuilder().create();
+        Retrofit retrofit = new RetrofitImplBuilder().create();
 
-    MemberAPI memberAPI = retrofit.create(MemberAPI.class);
+        MemberAPI memberAPI = retrofit.create(MemberAPI.class);
 
-    Call<MemberResponse> call = memberAPI.memberView("12");
-    call.enqueue(new Callback<MemberResponse>() {
-      @Override
-      public void onResponse(Call<MemberResponse> call, Response<MemberResponse> response) {
-        Log.i(TAG, "onResponse()");
-        if (response.isSuccessful()) {
-          Log.d(TAG, "onResponse: success");
-          memberResponse = response.body();
-          Log.d(TAG, "onResponse: " + memberResponse.toString());
-        } else {
-          Log.d(TAG, "onResponse: unsuccess");
-        }
-      }
+        final TextView textView = findViewById(R.id.textview1);
 
-      @Override
-      public void onFailure(Call<MemberResponse> call, Throwable t) {
-        Log.i(TAG, "onFailure()");
-        Log.w(TAG, t.getMessage(), t);
-      }
-    });
+        Call<MemberResponse> call = memberAPI.memberView("12");
+        call.enqueue(new Callback<MemberResponse>() {
+            @Override
+            public void onResponse(Call<MemberResponse> call, Response<MemberResponse> response) {
+                Log.i(TAG, "onResponse()");
+                if (response.isSuccessful()) {
+                    Log.d(TAG, "onResponse: success");
+                    memberResponse = response.body();
+                    Log.d(TAG, "onResponse: " + memberResponse.toString());
 
-    final Call<MemberResponse> callAsync = memberAPI.memberView("13");
+                    textView.setText(memberResponse.getMember().getEmail());
+                } else {
+                    Log.d(TAG, "onResponse: unsuccess");
+                }
+            }
 
-    new AsyncTask<Void, Void, Void>() {
-      @Override
-      protected Void doInBackground(Void... voids) {
-        try {
-          Response<MemberResponse> memberResponseResponse = callAsync.execute();
-          Log.d(TAG, "onCreate:memberResponseResponse " + memberResponseResponse.toString());
-          Log.d(TAG,
-              "doInBackground: memberResponseResponse.body().toString(): " + memberResponseResponse
-                  .body().toString());
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
-        return null;
-      }
-    }.execute();
+            @Override
+            public void onFailure(Call<MemberResponse> call, Throwable t) {
+                Log.i(TAG, "onFailure()");
+                Log.w(TAG, t.getMessage(), t);
+            }
+        });
 
-    Log.d(TAG, "onCreate: " + memberResponse);
 
-  }
+//        final TextView textView1 = findViewById(R.id.textview2);
+//        final Call<MemberResponse> callAsync = memberAPI.memberView("13");
+//
+//        new AsyncTask<Void, Void, Void>() {
+//
+//            Response<MemberResponse> memberResponseResponse;
+//
+//            @Override
+//            protected Void doInBackground(Void... voids) {
+//                try {
+//                    memberResponseResponse = callAsync.execute();
+//                    Log.d(TAG, "onCreate:memberResponseResponse " + memberResponseResponse.toString());
+//                    Log.d(TAG,
+//                            "doInBackground: memberResponseResponse.body().toString(): " + memberResponseResponse
+//                                    .body().toString());
+//
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//                return null;
+//            }
+//
+//            @Override
+//            protected void onPostExecute(Void aVoid) {
+//                super.onPostExecute(aVoid);
+//
+//                textView1.setText(memberResponseResponse.body().getMember().getEmail());
+//            }
+//        }.execute();
+
+        Log.d(TAG, "onCreate: " + memberResponse);
+
+    }
 }
--- app/src/main/java/com/goffity/mobile/android/demo/retrofitdemo/network/retrofit/RetrofitImplBuilder.java
+++ app/src/main/java/com/goffity/mobile/android/demo/retrofitdemo/network/retrofit/RetrofitImplBuilder.java
@@ -2,26 +2,32 @@ package com.goffity.mobile.android.demo.retrofitdemo.network.retrofit;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import android.util.Log;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitImplBuilder {
 
-  private static final String BASE_URL = "http://159.89.195.61:9080/";
+    //  private static final String BASE_URL = "http://159.89.195.61:9080/";
+    public static String BASE_URL = "http://127.0.0.11";
+    private String TAG = RetrofitImplBuilder.class.getSimpleName();
 
-  public Retrofit create() {
-    return create(BASE_URL);
-  }
+    public Retrofit create() {
+        Log.d(TAG, "create: " + BASE_URL);
+        return create(BASE_URL);
+    }
 
-  public Retrofit create(String BASE_URL) {
-    Gson gson = new GsonBuilder()
-        .setLenient()
-        .create();
+    public Retrofit create(String BASE_URL) {
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
 
-    return new Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create(gson))
-        .build();
-  }
+        return new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+    }
 
 }
